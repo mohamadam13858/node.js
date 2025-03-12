2665b4dba98be8ce9481b69aa4c822138d0f6839
mohamad <mh711748@gmail.com>
2025-03-13 03:24:43 +0330

    get-:id

--- app.js
+++ app.js
@@ -13,6 +13,21 @@ app.get('/api/users', (req, res) => {
     );
 })
 
+app.get('/api/users/:id', (req, res) => {
+   const Users = users.find(u=>u.id === parseInt(req.params.id))
+   if (Users) {
+       res.json({
+        data: Users,
+        message: "ok"
+       })
+   }else{
+    res.json({
+        data:null,
+        message: 'the user with the given id was not found'
+    })
+   }
+})
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}`));
