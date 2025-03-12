7f255bf55526072e470eeb57e34e59e523021fd4
mohamad <mh711748@gmail.com>
2025-03-13 02:55:49 +0330

    req.query

--- app.js
+++ app.js
@@ -5,21 +5,23 @@ const app = express();
 
 
 
-app.get('/api/users' , (req , res)=>{
-  res.send([
-    {id: 1 , name: 'user1'},
-    {id: 2 , name: 'user2'},
-  ])
+app.get('/api/users', (req, res) => {
+    console.log(req.query);
+    res.send([
+        { id: 1, name: 'user1' },
+        { id: 2, name: 'user2' },
+    ])
 })
-app.get('/api/users/:id' , (req , res)=>{
-    res.send({id: req.params.id , name: `user${req.params.id}`})    
+app.get('/api/users/:id', (req, res) => {
+    
+    res.send({ id: req.params.id, name: `user${req.params.id}` })
 })
 
 const port = process.env.PORT || 3000;
 
-app.listen(port , ()=>{
+app.listen(port, () => {
     console.log(`listening on port ${port}`);
-    
+
 })
 
 
