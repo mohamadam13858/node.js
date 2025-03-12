52c7909d7a3ca06c6f572710f2c95c26ecffaf62
mohamad <mh711748@gmail.com>
2025-03-13 02:31:17 +0330

    nodemon

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const express = require('express')
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('hello express')
+
+
+app.get('/api/users' , (req , res)=>{
+  res.send([
+    {id: 1 , name: 'user1'},
+    {id: 2 , name: 'user2'},
+  ])
 })
 
 app.listen(3000 , ()=>{
     console.log('listening on port 3000');
+    
 })
+
