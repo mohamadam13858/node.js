77707d3b0fe9da33595750ec3f240c202eb60300
mohamad <mh711748@gmail.com>
2025-03-13 19:01:07 +0330

    edit-user , put

--- app.js
+++ app.js
@@ -47,6 +47,36 @@ app.post('/api/users', [
 })
 
 
+
+app.put('/api/users/:id', [
+    body('email', 'email must be valid').isEmail(),
+    body('first_name', 'first name cant be empty').notEmpty(),
+    body('last_name', 'last name cant be empty').notEmpty(),
+], (req, res) => {
+    const  user = users.find(u => u.id == req.params.id)
+    if (!user) {
+        return res.status(404).json({
+            data:null,
+            message: "the user with the given id was not"
+        })
+    }
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ data: null, errors: errors.array(), message: "validation error" })
+    }
+    users = users.map(user=>{
+        if (user.id == req.params.id) {
+            return {...user , ...req.body}
+        }
+        return user
+    })
+    res.json({
+        data: users,
+        message: "ok"
+    })
+})
+
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}`));
 
