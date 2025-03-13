8abb44ab4c2b9699401242c137b9c500c8ca2be5
mohamad <mh711748@gmail.com>
2025-03-13 17:32:54 +0330

    add user - post

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 let users = require('./users')
-
 const app = express();
+app.use(express.json())
 
 
 app.get('/api/users', (req, res) => {
@@ -14,18 +14,27 @@ app.get('/api/users', (req, res) => {
 })
 
 app.get('/api/users/:id', (req, res) => {
-   const Users = users.find(u=>u.id === parseInt(req.params.id))
-   if (Users) {
-       res.json({
-        data: Users,
+    const Users = users.find(u => u.id === parseInt(req.params.id))
+    if (Users) {
+        res.json({
+            data: Users,
+            message: "ok"
+        })
+    } else {
+        res.status(404).json({
+            data: null,
+            message: 'the user with the given id was not found'
+        })
+    }
+})
+
+
+app.post('/api/users', (req, res) => {
+    users.push({id : users.length + 1 , ...req.body})
+    res.json({
+        data: users , 
         message: "ok"
-       })
-   }else{
-    res.status(404).json({
-        data:null,
-        message: 'the user with the given id was not found'
     })
-   }
 })
 
 
